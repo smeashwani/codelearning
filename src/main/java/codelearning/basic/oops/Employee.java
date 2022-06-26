2a1f09ad888276192bea558d874fb636f80d7568
smeashwani <104685977+smeashwani@users.noreply.github.com>
2022-06-26 13:34:59 +0530

    constructor

--- src/main/java/codelearning/basic/oops/Employee.java
+++ src/main/java/codelearning/basic/oops/Employee.java
@@ -1,9 +1,33 @@
 package codelearning.basic.oops;
 
 public class Employee {
+	
+	{
+		age =10;
+		salary=20000;
+		System.out.println("init block");
+	}
 	int age;
 	int salary;
 	String name;
+	static String companyName;
 	
+	static
+	{
+		companyName="Guest";
+		System.out.println("static init bloc");
+	}
+	
+	
+	{
+		age =18;
+		salary=18000;
+		System.out.println("init block2");
+	}
+	
+	Employee(){
+		System.out.println("cons default");
+	}
 
 }
+  
--- src/main/java/codelearning/basic/oops/Runner.java
+++ src/main/java/codelearning/basic/oops/Runner.java
@@ -1,29 +1,32 @@
 package codelearning.basic.oops;
 
 public class Runner {
+	static 
+	{
+		System.out.println("welcome");
+	}
+	int a=10;
+	static int b=20;
+	
 	public static void main(String[] args) {
-		Employee e = new Employee();
-		e.age=18;
-		e.salary=18000;
-		e.name="Guest";
-		System.out.println(e.age+ "-" + e.salary + e.name);
 		
-		Employee e1 = new Employee();
-		e1.age=17;
-		e1.salary=17000;
-		e1.name="Guest1";
-		System.out.println(e1.age+ "-" + e1.salary + e1.name);
-		
-		update(e1);
-		System.out.println(e.age+ "-" + e.salary + e.name);
-		System.out.println(e1.age+ "-" + e1.salary + e1.name);
+		System.out.println(a);
+		Employee e=new Employee();
+		Employee e1=new Employee();
+		System.out.println(Employee.companyName);
+//		e.salary=18000;
+//		
+//		Employee e1 = new Employee();
+//		e1.salary=19000;
+//		e1.companyName="ducat";
+//		Employee e2 = new Employee();
+//		e2.salary=20000;
+//		
+//		System.out.println(e.salary);
+//		System.out.println(e1.salary);
+//		System.out.println(e2.salary);
+//		System.out.println(e.companyName);
 	}
 
-	private static void update(Employee e1) {
-		e1.name="welcome";
-		e1 =  new Employee();
-		e1.age= 12;
-		e1.salary=12000;
-		
-	}
+ 
 }
