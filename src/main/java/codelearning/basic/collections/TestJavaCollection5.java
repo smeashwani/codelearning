7337249d0304146e5aba0b4d1cf0c18fd22278fb
smeashwani <104685977+smeashwani@users.noreply.github.com>
2022-08-20 12:20:53 +0530

    stream api

--- src/main/java/codelearning/basic/collections/TestJavaCollection5.java
+++ src/main/java/codelearning/basic/collections/TestJavaCollection5.java
@@ -1,16 +1,36 @@
 package codelearning.basic.collections;
 
-import java.util.HashMap;
-import java.util.IdentityHashMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class TestJavaCollection5 {
 	public static void main(String args[]) {
-		 IdentityHashMap m=new IdentityHashMap();
-		   Integer i1=new Integer(10);
-		   Integer i2=new Integer(10);
-		   m.put(i1,"pavan");
-		   m.put(i2,"kalyan");
-		   System.out.println(m);
+		List<Employee> stuJava =  new ArrayList();
+		stuJava.add(new Employee("A",18,22000));
+		stuJava.add(new Employee("B",22,22000));
+		stuJava.add(new Employee("c",21,22000));
+		Stream<Employee> filter = stuJava.stream().filter(emp -> emp.age > 20);
+		
+		stuJava.add(new Employee("d",22,22000));
+		stuJava.add(new Employee("e",21,22000));
+		
+		List<Employee> collect = filter.collect(Collectors.toList());
+		System.out.println(collect);
+//		List<String> collect = stuJava.stream().filter(emp -> emp.age > 20)
+//				.map(emp ->  emp.getName()).collect(Collectors.toList());
+//		System.out.println(collect);
+//		List<String> stuNet =  new ArrayList();
+//		stuJava.add("11");
+//		stuJava.add("22");
+//		stuJava.add("33");
+//		
+//		List<List<String>> stuLst = new ArrayList();
+//		stuLst.add(stuJava);
+//		stuLst.add(stuJava);
+//		
 
+		
 	}
 }
